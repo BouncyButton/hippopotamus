46d5343447710b290d70aee5dceccbeb9eb241d1
bouncybutton <luca.bergamin95@gmail.com>
2025-11-20 11:44:57 +0100

    debug

--- baselines/unetr_plus_plus/unetr_pp/network_architecture/general_purpose/model_components.py
+++ baselines/unetr_plus_plus/unetr_pp/network_architecture/general_purpose/model_components.py
@@ -62,13 +62,10 @@ class UnetrPPEncoder(nn.Module):
                 get_norm_layer(name=("group", {"num_groups": dims[i]}), channels=dims[i + 1]),
             )
             self.downsample_layers.append(downsample_layer)
-
-            x = torch.rand(1, 1, h, w, d)
-            x = self.stages[i](x)
+            x = downsample_layer(x)
             _, _, h, w, d = x.shape
-            if i < 4:
-                input_size[i + 1] = h * w * d
-                print(f"Calculated input size after downsample {i + 1}:", input_size[i + 1])
+            input_size[i + 1] = h * w * d
+            print(f"Calculated input size after downsample {i + 1}:", input_size[i + 1])
 
         self.stages = nn.ModuleList()  # 4 feature resolution stages, each consisting of multiple Transformer blocks
         for i in range(4):
