ab442ce54ce13746fd6e312c9a6096eb68b72739
bouncybutton <luca.bergamin95@gmail.com>
2026-01-27 13:16:34 +0100

    save nnunet run

---
+++ baselines/save_nnunet_run.py
@@ -0,0 +1,21 @@
+import wandb
+import argparse
+
+wandb.init(project="hippopotamus", entity="hippopotamus")
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--model-path', type=str, required=True, help='Path to the nnU-Net model checkpoint folder')
+parser.add_argument('--fold', type=int, required=True, help='Fold number of the nnU-Net model')
+parser.add_argument('--dataset', type=str, required=True, help='Dataset name the model was trained on')
+args = parser.parse_args()
+
+artifact = wandb.Artifact(
+    name=f"nnunet-model-{args.dataset}-fold{args.fold}",
+    type="model"
+)
+
+artifact.add_file(args.model_path)
+
+wandb.log_artifact(artifact)
+
+wandb.finish()
