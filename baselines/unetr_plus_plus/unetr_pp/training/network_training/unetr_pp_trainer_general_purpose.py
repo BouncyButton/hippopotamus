85acb854cffd862258f2a1a33fb8e35180b5da7c
bouncybutton <luca.bergamin95@gmail.com>
2026-02-06 15:02:05 +0100

    put 100 epochs for unetrpp

--- baselines/unetr_plus_plus/unetr_pp/training/network_training/unetr_pp_trainer_general_purpose.py
+++ baselines/unetr_plus_plus/unetr_pp/training/network_training/unetr_pp_trainer_general_purpose.py
@@ -46,7 +46,7 @@ class unetr_pp_trainer_general_purpose(TrainerGeneralPurpose):
                  unpack_data=True, deterministic=True, fp16=False, crop_size_x=64, crop_size_y=128, crop_size_z=128):
         super().__init__(plans_file, fold, output_folder, dataset_directory, batch_dice, stage, unpack_data,
                          deterministic, fp16)
-        self.max_num_epochs = 2
+        self.max_num_epochs = 100
         self.initial_lr = 1e-2
         self.deep_supervision_scales = None
         self.ds_loss_weights = None
